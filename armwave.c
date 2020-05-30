c506cc7215772aa517e7e17b4b959f2fd4d9bdb9
Tom Oldbury <toldbury@gmail.com>
2020-05-30 23:34:55 +0100

    performance tweaks

--- armwave.c
+++ armwave.c
@@ -318,8 +318,8 @@ void armwave_fill_pixbuf2(uint32_t *out_buffer)
             */
 
             xx = n % g_armwave_state.target_height;
-            yy = n / g_armwave_state.target_height;
-            offset = yy * g_armwave_state.target_width;//((xx * g_armwave_state.target_height) + yy);
+            yy = n / g_armwave_state.target_width;
+            offset = yy * g_armwave_state.target_height;//((xx * g_armwave_state.target_height) + yy);
             *(out_buffer_base + offset) = word;
 
             //printf("%d %d,%d (w:%d,h:%d)\n", n, xx, yy, g_armwave_state.target_width, g_armwave_state.target_height, offset * 4);
