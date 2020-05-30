489ce8c344b2c3392a262d813a4c78471f6add4c
Tom Oldbury <toldbury@gmail.com>
2020-05-30 13:48:00 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -220,7 +220,7 @@ void armwave_dump_ppm_debug(uint32_t *buffer, char *fn)
 
 	for(yy = 0; yy < g_armwave_state.target_height; yy++) {
 		for(xx = 0; xx < g_armwave_state.target_width; xx++) {
-			data = *(buffer + (xx + (yy * g_armwave_state.target_width));
+			data = *(buffer + (xx + (yy * g_armwave_state.target_width)));
 			//printf("xx,yy=%4d,%4d, word=0x%08x\n", xx, yy, data);
 
 			fprintf(fp, "%3d %3d %3d\n", (data >> 16) & 0xff, (data >> 8) & 0xff, data & 0xff);
