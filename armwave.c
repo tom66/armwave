4384b379db130fd7a4ec2900b9277ac4500837a5
Tom Oldbury <toldbury@gmail.com>
2020-05-30 13:24:08 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -179,11 +179,11 @@ uint32_t *armwave_create_pixbuf()
 
 #if 1
 	// Buffer is sent non-rotated: we use GDK/GL to assemble and rotate it
-	for(xx = 0; xx < g_armwave_state.target_width; xx++) {
-		for(yy = 0; yy < g_armwave_state.target_height; yy++) {
+	for(yy = 0; yy < g_armwave_state.target_height; yy++) {
+		for(xx = 0; xx < g_armwave_state.target_width; xx++) {
 			//printf("xx,yy=%d,%d, row_ptr=0x%08x\n", xx, yy, row_ptr);
 			//value = *(row_ptr + xx + (yy * g_armwave_state.target_width));
-			value = yy / 4; // *(row_ptr + xx);
+			value = xx / 4; // *(row_ptr + xx);
 			//printf("xx,yy=%d,%d, value=%d\n", xx, yy, value);
 
 			rr = g_armwave_state.ch1_color.r * value;  // We could also do a gamma LUT here
