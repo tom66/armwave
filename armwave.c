f058e3d3432c798483b6f14ec747df38cb2a9c8c
Tom Oldbury <toldbury@gmail.com>
2020-05-30 13:10:41 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -73,7 +73,9 @@ void render_nonaa_to_buffer_1ch_slice(uint32_t slice_y, uint32_t height)
 		// roll through y and render the slice into the out buffer
 		// buffer is rendered rotated by 90 degrees
 		for(yy = 0; yy < height; yy++) {
-			write_buffer = write_buffer_base + (g_armwave_state.xcoord_to_xpixel[slice_y + yy] * g_armwave_state.target_width);
+			//write_buffer = write_buffer_base + (g_armwave_state.xcoord_to_xpixel[slice_y + yy] * g_armwave_state.target_width);
+
+			write_buffer_base += g_armwave_state.target_width;
 
 #if 0
 			value = (*(wave_base + yy)) * g_armwave_state.vscale;
