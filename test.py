b08de6bda3293bd041bb948e873e542880fc4d7a
Tom Oldbury <toldbury@gmail.com>
2020-05-30 17:35:52 +0100

    progress towards Python test

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import armwave
 
+print("test_init()")
 armwave.test_init()
+
+print("test_generate()")
 armwave.test_generate()
+
 print(armwave.test_get_buffer())
+
+print("dump buffer to ppm")
 armwave.test_dump_buffer_to_ppm("test_python.ppm")
+
+print("cleanup")
 armwave.cleanup()
 
