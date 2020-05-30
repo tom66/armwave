223d10d69a3db7543e3cde80e19edb95f2cc1736
Tom Oldbury <toldbury@gmail.com>
2020-05-30 13:50:37 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -187,7 +187,7 @@ uint32_t *armwave_create_pixbuf()
 	for(yy = 0; yy < g_armwave_state.target_height; yy++) {
 		for(xx = 0; xx < g_armwave_state.target_width; xx++) {
 			//printf("xx,yy=%d,%d, row_ptr=0x%08x\n", xx, yy, row_ptr);
-			value = *(base_ptr + xx + (yy * g_armwave_state.target_width));
+			value = *(base_ptr + xx + (yy * g_armwave_state.target_height));
 			//value = xx / 8; // *(row_ptr + xx);
 			//printf("xx,yy=%d,%d, value=%d\n", xx, yy, value);
 
