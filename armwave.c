637625e1f9802b7ad830b3e4d4ab2ba5590be124
Tom Oldbury <toldbury@gmail.com>
2020-05-31 10:42:55 +0100

    scaled renderer

--- armwave.c
+++ armwave.c
@@ -326,9 +326,10 @@ void armwave_fill_pixbuf_scaled(uint32_t *out_buffer)
                     // Do line scaling as necessary.
                     nsub = n + i;
                     xx = nsub & 0xff;
+                    yy = nsub >> 8;
 
                     for(ysub = 0; ysub < vscale; ysub++) {
-                        yy = ((nsub >> 8) * vscale) + ysub;
+                        //yy = ((nsub >> 8) * vscale) + ysub;
                         offset = yy + (xx * g_armwave_state.target_width);
                         *(out_buffer_base + offset) = word;
                     }
