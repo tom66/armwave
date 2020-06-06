641e1798a9ce7ed72819bde9b6b7c84ef715ce53
Tom Oldbury <toldbury@gmail.com>
2020-06-06 21:00:38 +0100

    Debugging

--- armwave.c
+++ armwave.c
@@ -70,7 +70,7 @@ void render_nonaa_to_buffer_1ch_slice(uint32_t slice_y, uint32_t height)
     //write_buffer_base = g_armwave_state.ch1_buffer + (slice_y * g_armwave_state.bitdepth_height);
     write_buffer_base = g_armwave_state.ch1_buffer + (((slice_y * g_armwave_state.cmp_x_bitdepth_scale) >> AM_XCOORD_MULT_SHIFT) * g_armwave_state.bitdepth_height);
 
-    printf("wb=0x%08x b=0x%08x ch1=0x%08x off=%d slice_y=%d height=%d scale=%d bitdepth_height=%d\n", \
+    //printf("wb=0x%08x b=0x%08x ch1=0x%08x off=%d slice_y=%d height=%d scale=%d bitdepth_height=%d\n", \
         g_armwave_state.wave_buffer, write_buffer_base, g_armwave_state.ch1_buffer, \
         write_buffer_base - g_armwave_state.ch1_buffer, slice_y, height, g_armwave_state.cmp_x_bitdepth_scale, \
         g_armwave_state.bitdepth_height);
