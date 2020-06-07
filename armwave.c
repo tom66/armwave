d77a60e6f7d151077ff4454905732a9fe9638f2d
Tom Oldbury <toldbury@gmail.com>
2020-06-07 16:28:21 +0100

    Try to fix Y addressing in waveform gen for scaled waves

--- armwave.c
+++ armwave.c
@@ -182,8 +182,8 @@ void armwave_fill_pixbuf_scaled(uint32_t *out_buffer)
 
     assert(out_buffer != NULL);
 
-    // we don't really want to be doing this
-    //memset(out_buffer, 0x01, g_armwave_state.target_width * g_armwave_state.target_height * 4);
+    // we don't really want to be doing this if possible
+    memset(out_buffer, 0x00, g_armwave_state.target_width * g_armwave_state.target_height * 4);
 
     npix = g_armwave_state.target_width * 256; 
     //vscale = g_armwave_state.target_height >> 8;
