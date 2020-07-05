d2cf19bcf727ac71257a9f5e14e52e7efc22fe20
Tom Oldbury <toldbury@gmail.com>
2020-07-05 21:29:02 +0100

    Improvements

--- armwave.c
+++ armwave.c
@@ -294,6 +294,8 @@ void render_nonaa_to_buffer_1ch_slice(uint32_t slice_y, uint32_t height)
         write_buffer_base - g_armwave_state.ch1_buffer, slice_y, height, g_armwave_state.cmp_x_bitdepth_scale, \
         g_armwave_state.bitdepth_height, g_armwave_state.waves * g_armwave_state.wave_stride);
 
+    return;
+        
     // roll through each waveform
     for(w = 0; w < g_armwave_state.waves; w++) {
         wave_base = g_armwave_state.wave_buffer + slice_y + (w * g_armwave_state.wave_stride);
