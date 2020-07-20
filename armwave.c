805f45539f6e226ba7c3f6aa6a0ff4568432f6b9
Tom Oldbury <toldbury@gmail.com>
2020-07-20 20:36:44 +0100

    Development of trigger offset calculation

--- armwave.c
+++ armwave.c
@@ -294,7 +294,7 @@ void armwave_init()
 void render_nonaa_to_buffer_1ch_slice(uint32_t slice_y, uint32_t height)
 {
     static int test_toff = 0;
-    const int8_t trig_corr[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    const int8_t trig_corr[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
 
     int yy, ys, yi, w, scale_value, i, c, j, a, b, read, toff, rotate;
     uint32_t value, word;
